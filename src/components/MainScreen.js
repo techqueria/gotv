4c3e82d0b2428681311c9300882d42af21a311c5
Eddie Escamilla <emartinezescamilla@gmail.com>
2018-03-02 15:39:16 -0800

    - Add boilerplate components to mock login - Add base reducers - Add base appNavigator

---
+++ src/components/MainScreen.js
@@ -0,0 +1,31 @@
+/**
+ * Created by escamilla on 3/2/18.
+ */
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import LoginStatusMessage from './LoginStatusMessage';
+import AuthButton from './AuthButton';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+const MainScreen = () => (
+  <View style={styles.container}>
+    <LoginStatusMe1ssage />
+    <AuthButton />
+  </View>
+);
+
+MainScreen.navigationOptions = {
+  title: 'Home Screen/MainScreen',
+};
+
+export default MainScreen;
