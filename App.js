d6efeeb0036bae50938ae0005c1aa150af8e4193
Eddie Escamilla <emartinezescamilla@gmail.com>
2018-03-02 21:54:28 -0800

    - npm install redux thunk - add thunk to middleware

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
+import thunk from 'redux-thunk'
 import AppReducer from './src/reducers';
 import AppWithNavigationState from './src/navigators/AppNavigator';
 import { middleware } from './src/utils/redux';
 
 const store = createStore(
   AppReducer,
-  applyMiddleware(middleware),
+  applyMiddleware(middleware, thunk),
 );
 
 class EscamillaApp extends React.Component {
